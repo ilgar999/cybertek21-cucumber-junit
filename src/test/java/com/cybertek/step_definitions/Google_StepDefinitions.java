715eeaea4e09b2a48492900a5052e2ba874b6740
IlgarA <ilqar.nureddinoglu@gmail.com>
2021-02-12 17:11:00 -0500

    Cucumber project/ 02.12.2021/ 2nd day

--- src/test/java/com/cybertek/step_definitions/Google_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Google_StepDefinitions.java
@@ -17,28 +17,43 @@ public class Google_StepDefinitions {
         String expected = "Google";
 
         Assert.assertTrue(actual.equals(expected));
+
+        //All of the above done in one line.
+        //Assert.assertTrue(Driver.getDriver().getTitle().equals("Google"));
     }
+
 
     @Given("User is on Google home page")
     public void user_is_on_google_home_page() {
-
         Driver.getDriver().get("https://www.google.com");
     }
 
     @When("User searches for apple")
     public void user_searches_for_apple() {
-
         GoogleSearchPage googleSearchPage = new GoogleSearchPage();
         googleSearchPage.searchBar.sendKeys("apple" + Keys.ENTER);
     }
-
     @Then("User should see apple in the title")
     public void user_should_see_apple_in_the_title() {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedInTitle = "apple";
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
-        String actulTitle = Driver.getDriver().getTitle();
-        String expectedTitle = "apple";
-
-        Assert.assertTrue(actulTitle.contains(expectedTitle));
+        Driver.closeDriver();
     }
 
+    @When("User searches for {string}")
+    public void user_searches_for(String string) {
+        GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+        googleSearchPage.searchBar.sendKeys(string + Keys.ENTER);
+    }
+
+    @Then("User should see {string} in the title")
+    public void user_should_see_in_the_title(String string) {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedInTitle = string;
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+        Driver.closeDriver();
+    }
 }
